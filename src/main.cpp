4cb4df8b9af84c574c1ee82c287eebbf0c7f03ba
Ali Efe GÜR <ali.efe.gur@hotmail.com>
2025-12-29 12:53:06 +0300

    GLFW Window & Immediate Mode Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World" << std::endl;
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize!" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Tutorial", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glVertex2f( 0.0f, 0.5f);
+        glEnd();
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
