5b7afe79828411d62a9c2782f2d51a49afc3da95
Ali Efe GÜR <ali.efe.gur@hotmail.com>
2026-02-04 22:03:14 +0300

    REVISION: Cleaner main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 #include "Core/ShaderProgram.h"
-#include "Utils/File.h"
 #include "Core/Vertex.h"
 
 int main(int argc, char** argv)
@@ -35,9 +34,9 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    ShaderProgram shaderProgram("shaders/vertexShader.glsl", "shaders/fragmentShader.glsl");
+    const ShaderProgram shaderProgram("shaders/vertexShader.glsl", "shaders/fragmentShader.glsl");
 
-    const Vertex vertices[] = {
+    constexpr Vertex vertices[] = {
         { -0.5, -0.5, 0.0f, 1.0f, 1.0f, 1.0f },
         { 0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f },
         { 0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f }
@@ -53,28 +52,27 @@ int main(int argc, char** argv)
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)(3 * sizeof(float)));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<const void*>(0));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<const void*>(3 * sizeof(float)));
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
 
     // Transformation data
-    glm::mat4 model{ 1.0f };
     glm::vec3 position{ 0.0f };
     glm::vec3 scale{ 1.0f };
     float rotation = 0.0f;
 
     while (!glfwWindowShouldClose(window))
     {
-        double time = glfwGetTime();
+        const auto time = static_cast<float>(glfwGetTime());
 
         // Update transformations
-        position.x = sinf(static_cast<float>(time));
-        scale.x = scale.y = scale.z = abs(cosf(static_cast<float>(time)));
-        rotation = static_cast<float>(time) * 50.0f;
+        position.x = sinf(time);
+        scale.x = scale.y = scale.z = fabsf(cosf(time));
+        rotation = time * 50.0f;
 
         // Calculate model matrix
-        model = glm::scale(
+        glm::mat4 model = glm::scale(
             glm::rotate(
                 glm::translate(glm::mat4(1.0f), position), 
                 glm::radians(rotation), { 0.0f, 0.0f, 1.0f }
@@ -93,11 +91,11 @@ int main(int argc, char** argv)
         shaderProgram.Use();
 
         // Pass model matrix to the uniform
-        unsigned uModelLocation = glGetUniformLocation(shaderProgram, "uModel");
+        const int uModelLocation = glGetUniformLocation(shaderProgram, "uModel");
         glUniformMatrix4fv(uModelLocation, 1, GL_FALSE, &model[0][0]);
 
         // Pass color to the uniform in shader
-        unsigned int uColorLocation = glGetUniformLocation(shaderProgram, "uColor");
+        const int uColorLocation = glGetUniformLocation(shaderProgram, "uColor");
         glUniform3f(uColorLocation, color[0], color[1], color[2]);
 
         glBindVertexArray(VAO);
